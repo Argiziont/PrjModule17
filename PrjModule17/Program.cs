8687ec8d4965865810f5662a089ca5dfa9c1001d
Argiziont <tools33333@gmail.com>
2021-01-20 16:20:37 +0200

    Add project files.

---
+++ PrjModule17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FileMaintainer;
+
+namespace PrjModule17
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            FileManager.GatherFiles("File.txt", "File1.txt", "File2.txt");
+            FileManager.SplitFiles("File.txt", 2);
+            FileManager.DeleteSubstring("File.txt", "abd");
+        }
+    }
+}
